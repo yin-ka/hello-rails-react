e6e9f6169b4fababddb7bf37c23b943c7b2b6141
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-11-02 18:57:55 +0100

    Fix linters errors

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,12 +1,11 @@
 class Api::V1::GreetingsController < ApplicationController
-    def index
-      @random_greeting = Greeting.order('RANDOM()').first
-  
-      if @random_greeting
-        render json: { greeting: @random_greeting.message }
-      else
-        render json: { greeting: "No greetings found" }
-      end
+  def index
+    @random_greeting = Greeting.order('RANDOM()').first
+
+    if @random_greeting
+      render json: { greeting: @random_greeting.message }
+    else
+      render json: { greeting: 'No greetings found' }
     end
   end
-  
+end
